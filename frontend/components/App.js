824a7fc9216cab441967ded72c0c7991d3301dee
Justyn Silva <justynhagen@gmail.com>
2024-03-04 21:42:22 -0600

    Confirmed that my code is working correctly and updating the local server as intended. I will now begin to complete my assignment

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const api_key = 'SBns2vnidYwZuWpU63xvghIGnOjVad7FBrHGEB3D'
+const api_key = 'DEMO_KEY'
 const URL = `https://api.nasa.gov/planetary/apod?api_key=${api_key}`
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         })
     }
     // fetchPhoto()
-    setApod([{
+    setApod({
       "copyright": "\nN. D. Liao\n",
       "date": "2024-03-04",
       "explanation": "What's happening across that field?  Pictured here are not auroras but nearby light pillars, a phenomenon typically much closer.   In most places on Earth, a lucky viewer can see a Sun pillar, a column of light appearing to extend up from the Sun caused by flat fluttering ice-crystals reflecting sunlight from the upper atmosphere.  Usually, these ice crystals evaporate before reaching the ground.  During freezing temperatures, however, flat fluttering ice crystals may form near the ground in a f...",
@@ -27,13 +27,16 @@ function App() {
       "service_version": "v1",
       "title": "Light Pillars Over Inner Mongolia",
       "url": "https://apod.nasa.gov/apod/image/2403/PillarsMongolia_Liao_960.jpg"
-    }])
+    })
   }, [])
+
+  if (!apod) return 'Fetching Photo of the Day...'
   return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
+    <section>
+      <div className='card'>
+        <h2>{apod.title}</h2>
+      </div>
+    </section>
   )
 }
 
